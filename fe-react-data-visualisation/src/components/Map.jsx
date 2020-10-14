d79ad4f283ea02449320a79ee8b05f6465efc9ba
Alex Brown <macuser@Macs-iMac.local>
2020-10-14 17:39:33 +0100

    remove hard-coded pin of london and popup message

--- fe-react-data-visualisation/src/components/Map.jsx
+++ fe-react-data-visualisation/src/components/Map.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import React from "react";
+import { Map, Marker, TileLayer } from "react-leaflet";
 
-
-const position = [51.505, -0.09]
+const position = [51.505, -0.09];
 const WorldMap = (props) => {
- return ( <div className='mapid'>
-    <Map center={position} zoom={0}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-      />
-      <Marker position={position}>
-        <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
-      </Marker>
-      {props.lat ? <Marker position={[props.lat, props.long]}></Marker>: undefined}
-    </Map>
+  return (
+    <div className="mapid">
+      <Map center={position} zoom={0}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {props.lat ? (
+          <Marker position={[props.lat, props.long]}></Marker>
+        ) : undefined}
+      </Map>
     </div>
-  )}
+  );
+};
 
-export default WorldMap
+export default WorldMap;
